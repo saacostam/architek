588272b1998346ffff392064e54463c994a6fa1e
saacostam <73088662+saacostam@users.noreply.github.com>
2025-11-15 02:37:11 -0500

    feat(navbar): add tooltip to theme call-to-action

--- src/features/layouts/ui/components/navbar.tsx
+++ src/features/layouts/ui/components/navbar.tsx
@@ -2,7 +2,7 @@ import { useAdapters } from "@/shared/adapters/core/app";
 import { RouteName } from "@/shared/adapters/navigation/domain";
 import { Logo } from "@/shared/components";
 import { MoonIcon, SunIcon } from "@/shared/icons";
-import { Flex, IconButton } from "@radix-ui/themes";
+import { Flex, IconButton, Tooltip } from "@radix-ui/themes";
 import { useCallback } from "react";
 import { Link } from "react-router";
 
@@ -16,21 +16,23 @@ export function Navbar() {
     }, [themeAdapter]);
 
     return <div style={{ backgroundColor: "var(--accent-1)", borderBottom: "1px solid var(--gray-6)" }}>
-        <Flex p="4" justify="between">
+        <Flex p="3" justify="between">
             <Link className="clean-link" to={navigationAdapter.generateRoute({ name: RouteName.HOME })}>
                 <Logo />
             </Link>
             <Flex align="center">
-                <IconButton
-                    onClick={onSwitchTheme}
-                    variant="ghost"
-                >
-                    {
-                        themeAdapter.theme === "light"
-                            ? <SunIcon height={24} width={24}/>
-                            : <MoonIcon height={24} width={24}/>
-                    }
-                </IconButton>
+                <Tooltip content="Toggle Theme">
+                    <IconButton
+                        onClick={onSwitchTheme}
+                        variant="ghost"
+                    >
+                        {
+                            themeAdapter.theme === "light"
+                                ? <SunIcon height={24} width={24}/>
+                                : <MoonIcon height={24} width={24}/>
+                        }
+                    </IconButton>
+                </Tooltip>
             </Flex>
         </Flex>
     </div>
